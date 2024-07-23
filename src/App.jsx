dc6b483cfd30371937d9ec276eb34afe8bfa16ea
elsacodesid <elsacodesid@gmail.com>
2024-07-23 16:52:51 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
 
   return (
     <>
-    
+    <div>Hello World</div>
+    <Button>What's up</Button>
     </>
   )
 }
